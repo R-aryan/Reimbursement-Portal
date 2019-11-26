c51cbd97149bb7039735f69c89bd60f404908251
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2019-11-26 10:06:23 +0530

    added web api folder

---
+++ Reimbursement-WebApi/ReimbursementShared/SharedDTO/EmployeeDataDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReimbursementShared.SharedDTO
+{
+    
+        public class EmployeeDataDTO
+        {
+            
+            public string FullName { get; set; }
+
+            
+            public string Email { get; set; }
+
+            
+            public string BankName { get; set; }
+
+           
+            public string AccountNymber { get; set; }
+
+           
+            public string PanNumber { get; set; }
+
+           
+            public string Password { get; set; }
+
+            public int ID { get; set; }
+
+        }
+    
+}
---
+++ Reimbursement-WebApi/ReimbursementShared/SharedDTO/ReimbursementDataDTO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReimbursementShared.SharedDTO
+{
+    public class ReimbursementDataDTO
+    {
+      
+        public int ID { get; set; }
+
+      
+        public string Email { get; set; }
+
+       
+        public string ReimbursementType { get; set; }
+
+        public string Currency { get; set; }
+
+       
+        public string RequestedValue { get; set; }
+
+        public string ApprovedValue { get; set; }
+
+      
+        public string RequestPhase { get; set; }
+
+
+        public string ReciptAttached { get; set; }
+
+       
+        public string DateString { get; set; }
+
+        public DateTime? ClaimDate { get; set; }
+    }
+}
