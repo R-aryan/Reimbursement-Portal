a10c1e9a7621167b911f43a69ae516fdf00e5444
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2019-12-03 09:31:35 +0530

    added web layer

---
+++ Reimbursement-WebApi/Reimbursement_Portal/Models/BankNamesViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Reimbursement_Portal.Models
+{
+    public class BankNamesViewModel
+    {
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Key]
+        public int Id { get; set; }
+    }
+}
